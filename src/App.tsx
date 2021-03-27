a350444391041997926a5cbe42edb4126fcfc999
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-03-27 13:20:42 +0700

    react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 
+export interface Foods {
+  id: number;
+  foodName: string;
+  review: number;
+  imageData: string;
+}
+
+const getFoods = async (): Promise<Foods[]> => {
+  return await (await (fetch('https://foodgenerateapi.herokuapp.com/api/foods'))).json();
+}
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<Foods[]>(
+    'foods',
+    getFoods
+  )
+  console.log(data);
   
   return (
     <div>Hello WOrld</div>
