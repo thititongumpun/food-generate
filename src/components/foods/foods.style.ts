9f8e3d4c718fbb9fb330698cd6605698df6db542
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-03-27 17:53:28 +0700

    button

--- src/components/foods/foods.style.ts
+++ src/components/foods/foods.style.ts
@@ -12,8 +12,7 @@ export const Wrapper = styled.div`
     height: 250px;
     width: 100%;
     object-fit: cover;
-    border-radius: 20px 20px 0 0;
-    border: 1px;  
+    border-bottom: 1px solid;
   }
 
   div {
