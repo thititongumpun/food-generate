4030b2c2551f086d5a83bd4096e857728ec01653
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-03-30 20:51:49 +0700

    roll back

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,8 @@
-import { useState } from 'react';
-import { useQuery } from 'react-query';
 
 
-import { ThemeProvider } from 'styled-components';
-import { useTheme } from './Theme';
-import Grid from '@material-ui/core/Grid';
-import { LinearProgress } from '@material-ui/core';
-
-import {Wrapper, Button} from './App.styles';
-import { ResponsiveDrawer } from './components/drawer/drawer';
-
-export interface Foods {
-  id: number;
-  foodName: string;
-  liked: number;
-  imageData: string;
-}
-
-const getFoods = async (): Promise<Foods[]> => {
-  return await (await (fetch('https://foodgenerateapi.herokuapp.com/api/foods'))).json();
-}
 
 const App: React.FC = () => {
-  const theme = useTheme();
-
-  const { data, isLoading, error } = useQuery<Foods[]>(
-    'foods',
-    getFoods
-  )
-  console.log(data);
-
-  if(error) return <div>....error.....</div>
-  if(isLoading) return <LinearProgress />;
-  
-  return (
-    <ThemeProvider theme={{ mode: theme.mode }}>
-      <Wrapper>
-        <Grid container spacing={3}>
-          {data?.map((food) => (
-            <Grid item key={food.id} xs={12} sm={4}>
-              <ResponsiveDrawer food={food}/>
-            </Grid>
-          ))}
-        </Grid>
-        <div className="buttons">
-        <Button onClick={() => theme.toggle()}>
-          {theme.mode === 'dark' ? 'Light Mode' : 'Dark Mode'}
-        </Button>
-        </div>
-      </Wrapper>
-    </ThemeProvider>
-  );
+  return (<div>Come Back to Basic</div>);
 }
-
-
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,8 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ThemeManager } from './Theme';
 
-const client = new QueryClient();
 
 ReactDOM.render(
-  <ThemeManager>
-    <QueryClientProvider client={client}>
-      <App />
-    </QueryClientProvider>
-  </ThemeManager>,
+      <App />,
   document.getElementById('root')
 );
